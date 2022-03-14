954f7fc3d198ac36e3cc690e3f69e684d18328f7
George Huber <36140322+GeorgeHuber@users.noreply.github.com>
2022-03-14 15:04:55 -0500

    added documentation

--- circle.js
+++ circle.js
@@ -1,13 +1,19 @@
-convertToCentered = (point) =>{
-    point.x += width/2;
-    point.y += height/2;
-    return point;
-}
 
 class FourierCircle{
     /*
         Circle(center: {x:?, y:?}, radius=?, freq=?hz, parent=? (reference))
     
+        The circle completes (hz) rotatations every second
+        If attatched on the end of another circle, put the variable with the other circle in the parent reference.
+
+        options =  {
+            shift:0, //in degrees
+            freq:0.5, //rotations per second
+            color:color(255,255,255), //rgb values
+            size: 4 //in pixels
+        }
+
+
     */
     constructor(center={x:0, y:0}, radius=50, options={}, parent=null){
         
@@ -15,8 +21,6 @@ class FourierCircle{
         this.color = options.color?options.color:color(255, 255, 255);
         this.shift = options.shift?2*Math.PI-options.shift*2*Math.PI/360:0;
         this.size = options.size?options.size:4;
-
-
 
         this.radius = radius;
         this.center = center;
@@ -27,6 +31,7 @@ class FourierCircle{
         this.shift = 1/this.freq*this.shift;
     }
 
+    //returns the center of the circle in the form {x:0, y:0}
     getCenter(t){
         let pointx = this.center.x;
         let pointy = this.center.y;
@@ -39,6 +44,7 @@ class FourierCircle{
 
     }
 
+    //returns the current point of the circle being drawn in the form {x:0, y:0}
     getPoint(t){
         let pointx = this.radius * Math.cos(t*this.freq*2*Math.PI+this.shift) +this.center.x;
         let pointy = this.radius * Math.sin(t*this.freq*2*Math.PI+this.shift) +this.center.y;
@@ -52,6 +58,7 @@ class FourierCircle{
         return {x:pointx,y:pointy};
     }
 
+    //draws the current point on the circle for given time
     drawPoint(t){
         let point = this.getPoint(t);
         //changes color
@@ -60,6 +67,7 @@ class FourierCircle{
         ellipse(point.x, point.y, this.size*2, this.size*2);
     }
 
+    //draws the whole circle outline
     drawCircle(t){
         let point = this.getCenter(t)
         noFill();
--- sketch.js
+++ sketch.js
@@ -1,32 +1,43 @@
-//const FourierCircle = require("./circle");
+let circles = []; //the array of circles being drawn
+let fps = 60; //frames per second
+let frameNumber = 0; //do not change, tracks elapsed time
+let trail = []; //the wavy thing being drawn off to the side
 
-let circles = [];
-let fps = 60;
-let frameNumber = 0;
-let trail =[];
+// defines the size of the canvas
 let width = 1200;
 let height = 800;
 
+//the circle which defines the graph. typically the outermost ones
 let trailCircle;
 
 
 
 function setup() {
+    //creates canvas
     createCanvas(width, height);
     frameRate(fps)
-    background(0);
-    
+
+    //sets background color
+    background(color(0,0,0));
+
+
+    //delete these circles and put your code below ->
+
+
+    //code to define a circle
     let circle1 = new FourierCircle(
-            {x:-height/2,y:0}, 
-            200,
+            {x:-height/2,y:0}, //center of the circle
+            200, //radius
             {
                 freq:.2,
                 shift:180,
                 color: color(200,200,255),
                 size: 10
             },
-            null
+            null //parent reference
         )
+
+    //makes more circles
     let circle2 = new FourierCircle(
         {x:0,y:0}, 
         100,
@@ -49,30 +60,18 @@ function setup() {
         },
         circle2
     )
-    
-    let circle4 = new FourierCircle(
-        {x:0,y:0}, 
-        25,
-        {
-            freq:1.2,
-            shift:180,
-            color: color(200,255,255),
-            size: 8
-        },
-        circle3
-    )
 
+    //adds them to the list of things being drawn
     circles.push(circle1);
     circles.push(circle2);
     circles.push(circle3);
-    circles.push(circle4)
 
-    trailCircle=circle4;
+    //graphs based on last circle
+    trailCircle=circle3;
 }
 
-
+//a function to draw all of the circles in the list
 function drawCircles(t){
-    
     for (var i =0; i<circles.length;i++){
         let circle = circles[i];
         circle.drawCircle(t);
@@ -83,6 +82,7 @@ function drawCircles(t){
     }
 }
 
+//draws the graph
 function drawTrail(t){
     for (var i =0; i<trail.length;i++){
         let point = trail[i];
@@ -99,23 +99,27 @@ function drawTrail(t){
 
 }
 
+//
 function draw() {
-    //centers coordinates
+    //centers coordinates and keeps time. Probably don't change this.
     translate(width/2, height/2);
-
-
 
     let t = frameNumber/fps;
     frameNumber+=1;
 
     //resets canvas
-    background(0);
+    background(color(0,0,0));
 
+    //adds current point to the graph
     let point = trailCircle.getPoint(t);
     let trailpoint = {x:width*1/6*0, y: point.y}
     trail.push(trailpoint)
+
+
+    //display line to graph from outer circle
     stroke(255);
     line(point.x, point.y, trailpoint.x,trailpoint.y)
+
     drawCircles(t);
     drawTrail(t)
 }
